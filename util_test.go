242a3a714e398d1e6e2a62ac0e7132fc0f826bf1
SAI NAGESWAR SATCHIDANAND <sainageswar@gmail.com>
2018-07-26 20:10:11 +0530

    Adding tests

---
+++ util_test.go
@@ -0,0 +1,21 @@
+package gospars
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestMergeMaps(t *testing.T) {
+	map1 := map[string]string {
+		"hello": "world" }
+	map2 := map[string]string {
+		"Go": "routines" }
+
+	expectedResult := map[string]string {
+		"hello": "world",
+		"Go": "routines" }
+	map3 := MergeMaps(map1, map2)
+	if !reflect.DeepEqual(map3, expectedResult) {
+		t.Error("map3 and expectedResult should be equal")
+	}
+}
